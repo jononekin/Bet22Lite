b0d5cbd18f251cccd87294f96a76b4f082be3cd9
Mateo Aguirre <zaguirrez05@gmail.com>
2023-10-08 20:44:51 +0200

    correcting static errors

--- src/main/java/domain/Apustua.java
+++ src/main/java/domain/Apustua.java
@@ -2,6 +2,7 @@ package domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,7 +14,7 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @Entity
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Apustua implements Serializable{
+public class Apustua implements Serializable {
 
 	/**
 	 * 
@@ -22,22 +23,23 @@ public class Apustua implements Serializable{
 
 	@XmlID
 	@XmlJavaTypeAdapter(IntegerAdapter.class)
-	@Id @GeneratedValue
-	private Integer apustuaNumber; 
-	
+	@Id
+	@GeneratedValue
+	private Integer apustuaNumber;
+
 	@XmlIDREF
 	private Quote kuota;
 	@XmlIDREF
 	private ApustuAnitza apustuAnitza;
 	private String egoera;
-	
+
 	public Apustua(ApustuAnitza ap, Quote q) {
 		super();
-		this.apustuAnitza=ap;
-		this.kuota=q;
-		this.egoera="jokoan";
+		this.apustuAnitza = ap;
+		this.kuota = q;
+		this.egoera = "jokoan";
 	}
-	
+
 	public Apustua() {
 		super();
 	}
@@ -65,7 +67,7 @@ public class Apustua implements Serializable{
 	public void setEgoera(String egoera) {
 		this.egoera = egoera;
 	}
-	
+
 	public ApustuAnitza getApustuAnitza() {
 		return apustuAnitza;
 	}
@@ -73,25 +75,26 @@ public class Apustua implements Serializable{
 	public void setApustuAnitza(ApustuAnitza apustuAnitza) {
 		this.apustuAnitza = apustuAnitza;
 	}
-	
+
 	public boolean galdutaMarkatu(Quote quo) {
-		if(Objects.equals(kuota.getQuestion().getQuestionNumber(), quo.getQuestion().getQuestionNumber()) && quo.getQuoteNumber()!=kuota.getQuoteNumber()) {
-			this.egoera="galduta";
+		if (Objects.equals(kuota.getQuestion().getQuestionNumber(), quo.getQuestion().getQuestionNumber())
+				&& !Objects.equals(quo.getQuoteNumber(), kuota.getQuoteNumber())) {
+			this.egoera = "galduta";
 			return true;
 		}
 		return false;
 	}
-	
+
 	public void eguneratuApustuKant(Sport s) {
 		s.eguneratuApustuKantitatea();
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
-		Apustua a = (Apustua) o; 
-		if(a==null) {
+		Apustua a = (Apustua) o;
+		if (a == null) {
 			return false;
 		}
-		return this.getApostuaNumber().equals(a.getApostuaNumber()); 
+		return this.getApostuaNumber().equals(a.getApostuaNumber());
 	}
 }
