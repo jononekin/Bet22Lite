090fc283aba470531c7ae4125189f15af38afe57
Agustin <76200509+AgusZ02@users.noreply.github.com>
2023-10-14 18:13:57 +0200

    Update EmaitzakIpiniDAB.java

--- src/test/java/EmaitzakIpiniDAB.java
+++ src/test/java/EmaitzakIpiniDAB.java
@@ -41,6 +41,7 @@ public class EmaitzakIpiniDAB {
 	public void test2(){
 		Event e =testDA.addEventWithQuestion("testEvent", new Date(), "testQuestion", 1);
 		Quote q = testDA.addQuotesTo(e.getQuestions().get(0), 1, "testQuote");
+		
 		try {
 			sut.EmaitzakIpini(q);
 			Question question = sut.findQuestionFromQuote(q);
@@ -54,61 +55,4 @@ public class EmaitzakIpiniDAB {
 			testDA.removeEvent(e);
 		}
 	}
-
-	@Test
-	public void test3(){
-		Event e =testDA.addEventWithQuestion("testEvent", new Date(), "testQuestion", 1);
-		Quote q = testDA.addQuotesTo(e.getQuestions().get(0), 1, "testQuote");
-		Registered u = testDA.addUser("testUser");
-		Apustua ap = testDA.addApuestaTo(q, u, 1);
-		
-
-		try {
-			sut.EmaitzakIpini(q);
-			Question question = sut.findQuestionFromQuote(q);
-			assertNotNull(question);
-			Apustua apuesta = question.getQuotes().get(0).getApustuak().get(0);
-			assertNotNull(apuesta);
-			assertEquals(e.getQuestions().get(0).getQuestionNumber(), question.getQuestionNumber());
-			assertEquals(q.getForecast(),question.getResult());
-			assertEquals("irabazita",apuesta.getEgoera());
-			
-		} catch (Exception ex) {
-			fail("No debería de dar error");
-		} finally{
-			
-			assertTrue(testDA.removeUser(u.getUsername()));
-			assertTrue(testDA.removeEvent(e));
-		}
-	}
-
-	@Test
-	public void test4(){
-		Event e =testDA.addEventWithQuestion("testEvent", new Date(), "testQuestion", 1);
-		Quote q = testDA.addQuotesTo(e.getQuestions().get(0), 1, "testQuote");
-		Quote q2 = testDA.addQuotesTo(e.getQuestions().get(0), 2, "testQuote2");
-		Registered u = testDA.addUser("testUser");
-		Apustua ap = testDA.addApuestaTo(q, u, 1);
-		Apustua ap2 = testDA.addApuestaTo(q2, u, 12);
-		
-
-		try {
-			sut.EmaitzakIpini(q);
-			Question question = sut.findQuestionFromQuote(q2);
-			assertNotNull(question);
-			Apustua apuesta = question.getQuotes().get(0).getApustuak().get(0);
-			assertNotNull(apuesta);
-			assertEquals(e.getQuestions().get(0).getQuestionNumber(), question.getQuestionNumber());
-			assertNotEquals(q2.getForecast(),question.getResult());
-			assertEquals("galduta",apuesta.getEgoera());
-			
-		} catch (Exception ex) {
-			fail("No debería de dar error");
-		} finally{
-			
-			// assertTrue(testDA.removeEvent(e));
-			// assertTrue(testDA.removeUser(u.getUsername()));
-		}
-	}
-
 }
