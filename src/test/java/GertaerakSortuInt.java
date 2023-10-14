7b7c9fedb81644316c3facadbf579a739c3e4096
Olarizu <olarizuga@gmail.com>
2023-10-14 20:49:38 +0200

    Update GertaerakSortuInt.java

--- src/test/java/GertaerakSortuInt.java
+++ src/test/java/GertaerakSortuInt.java
@@ -1,129 +1,212 @@
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
+import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 import dataAccess.DataAccess;
-import domain.Event;
-import domain.Question;
 import exceptions.EventFinished;
-import exceptions.QuestionAlreadyExist;
-import test.businessLogic.TestFacadeImplementation;
 
 public class GertaerakSortuInt {
-	static BLFacadeImplementation sut;
-	static TestFacadeImplementation testBL;
+	DataAccess dataAccess = Mockito.mock((DataAccess.class));
+	BLFacade sut=new BLFacadeImplementation(dataAccess);
 
 	
-	
-	@BeforeClass
-	public static void setUpClass() {
-		//sut= new BLFacadeImplementation();
-		
-		// you can parametrize the DataAccess used by BLFacadeImplementation
-		//DataAccess da= new DataAccess(ConfigXML.getInstance().getDataBaseOpenMode().equals("initialize"));
-		DataAccess da= new DataAccess();
-
-		sut=new BLFacadeImplementation(da);
-		
-		testBL= new TestFacadeImplementation();
-	}
 	
 	@Test
 	//sut.createQuestion:  The event has one question with a queryText. 
 	public void test1() {
+		
+	
+		//define paramaters
+		String deporte="fusbol";
+		String des="Atletico-Athletic";
+		boolean resp;
+	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date oneDate=null;
 		try {
-			
-			//define paramaters
-			String des = ""
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date oneDate=null;;
-			try {
-				oneDate = sdf.parse("05/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			
-			//configure the state of the system (create object in the dabatase)
-			ev = testBL.addEventWithQuestion(eventText,oneDate,queryText,betMinimum );
-			
-			
-			//invoke System Under Test (sut)  
-			sut.createQuestion(ev, queryText, betMinimum);
-			
-			
-			//if the program continues fail
-		    fail();
-		   } catch (QuestionAlreadyExist e) {
+			oneDate = sdf.parse("05/10/2022");
+		} catch (ParseException e) {
 			// TODO Auto-generated catch block
-			// if the program goes to this point OK  
-			assertTrue(true);
-			} catch (EventFinished e) {
-				// if the program goes to this point fail
-			    fail();
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				  //Remove the created objects in the database (cascade removing)   
-		          boolean b=testBL.removeEvent(ev);
-		           System.out.println("Finally "+b);          
-		        }
-		   }
+			e.printStackTrace();
+		}	
+		
+		try {
+			//configure Mock
+			Mockito.doReturn(false).when(dataAccess).gertaerakSortu(Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
+			
+			//invoke System Under Test(sut)
+			resp = sut.gertaerakSortu(des, oneDate, deporte);
+			
+			//verify the results
+			assertFalse(resp);
+			
+			ArgumentCaptor<String> desCaptor = ArgumentCaptor.forClass(String.class);
+			ArgumentCaptor<Date> oneDateCaptor = ArgumentCaptor.forClass(Date.class);
+			ArgumentCaptor<String> deporteCaptor = ArgumentCaptor.forClass(String.class);
+			
+			Mockito.verify(dataAccess,Mockito.times(1)).gertaerakSortu(desCaptor.capture(), oneDateCaptor.capture(), deporteCaptor.capture());
+			
+			assertEquals(desCaptor.getValue(),des);
+			assertEquals(oneDateCaptor.getValue(),oneDate);
+			assertEquals(deporteCaptor.getValue(),deporte);
+			
+			
+		} catch (EventFinished e) {
+			fail();
+		}
+			
+		
+	}
+	
+			
 	
 	@Test
 	//sut.createQuestion:  The event has NOT one question with a queryText. 
 	public void test2() {
+		
+		//define paramaters
+		String deporte="Futbol";
+		String des="Atletico-Athletic";
+		boolean resp;
+	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date oneDate=null;
 		try {
+			oneDate = sdf.parse("05/10/2022");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		try {
+			//configure mock
+			Mockito.doReturn(true).when(dataAccess).gertaerakSortu(Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
 			
-			//define paramaters
-			String eventText="event1";
-			String queryText="query1";
-			Float betMinimum=new Float(2);
+			//invoke System Under Test(sut)
+			resp = sut.gertaerakSortu(des, oneDate, deporte);
 			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date oneDate=null;;
-			try {
-				oneDate = sdf.parse("05/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
+			//verify results
+			assertTrue(resp);
 			
-			//configure the state of the system (create object in the dabatase)
-			ev = testBL.addEventWithQuestion(eventText,oneDate,"query2",betMinimum );
+			ArgumentCaptor<String> desCaptor = ArgumentCaptor.forClass(String.class);
+			ArgumentCaptor<Date> oneDateCaptor = ArgumentCaptor.forClass(Date.class);
+			ArgumentCaptor<String> deporteCaptor = ArgumentCaptor.forClass(String.class);
 			
-			//invoke System Under Test (sut)  
-			Question q=sut.createQuestion(ev, queryText, betMinimum);
+			Mockito.verify(dataAccess,Mockito.times(1)).gertaerakSortu(desCaptor.capture(), oneDateCaptor.capture(), deporteCaptor.capture());
 			
+			assertEquals(desCaptor.getValue(),des);
+			assertEquals(oneDateCaptor.getValue(),oneDate);
+			assertEquals(deporteCaptor.getValue(),deporte);
+			
+		} catch (Exception e) {
+			fail();
+		}
+			
+	}
+	
+	@Test
+	//sut.createQuestion:  The event has NOT one question with a queryText. 
+	public void test3() {
+		
+			
+		//define paramaters
+		String deporte="Futbol";
+		String des="Atletico-Athletic";
+		
+	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date oneDate=null;
+		try {
+			oneDate = sdf.parse("25/10/2022");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		try {
+			//configure mock
+			Mockito.doReturn(true).when(dataAccess).gertaerakSortu(Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
+			
+			//invoke System Under Test(sut)
+			boolean resp = sut.gertaerakSortu(des, oneDate, deporte);
+			
+			
+			//verify results
+			ArgumentCaptor<String> desCaptor = ArgumentCaptor.forClass(String.class);
+			ArgumentCaptor<Date> oneDateCaptor = ArgumentCaptor.forClass(Date.class);
+			ArgumentCaptor<String> deporteCaptor = ArgumentCaptor.forClass(String.class);
+			
+			Mockito.verify(dataAccess, Mockito.times(0)).gertaerakSortu(desCaptor.capture(), oneDateCaptor.capture(), deporteCaptor.capture());
+			
+			assertEquals(desCaptor.getValue(),des);
+			assertEquals(oneDateCaptor.getValue(),oneDate);
+			assertEquals(deporteCaptor.getValue(),deporte);
+			
+		} catch (EventFinished e) {
+			assertTrue(true);
+		}
+			
+	}
+	
+	@Test
+	//sut.createQuestion:  The event has NOT one question with a queryText. 
+	public void test4() {
+		
+		
+		//define paramaters
+		String deporte="fusbol";
+		String des="Atletico-Athletic";
+		boolean resp;
+	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date oneDate=null;
+		try {
+			oneDate = sdf.parse("05/10/2022");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		try {
+			//configure Mock
+			Mockito.doReturn(false).when(dataAccess).gertaerakSortu(Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
+			
+			//invoke System Under Test(sut)
+			resp = sut.gertaerakSortu(des, oneDate, deporte);
 			
 			//verify the results
-			assertTrue(q!=null);
-			assertEquals(q.getQuestion(),queryText);
-			assertEquals(q.getBetMinimum(),betMinimum,0);
+			assertFalse(resp);
+			
+			ArgumentCaptor<String> desCaptor = ArgumentCaptor.forClass(String.class);
+			ArgumentCaptor<Date> oneDateCaptor = ArgumentCaptor.forClass(Date.class);
+			ArgumentCaptor<String> deporteCaptor = ArgumentCaptor.forClass(String.class);
+			
+			Mockito.verify(dataAccess,Mockito.times(1)).gertaerakSortu(desCaptor.capture(), oneDateCaptor.capture(), deporteCaptor.capture());
+			
+			assertEquals(desCaptor.getValue(),des);
+			assertEquals(oneDateCaptor.getValue(),oneDate);
+			assertEquals(deporteCaptor.getValue(),deporte);
 			
 			
-		   } catch (QuestionAlreadyExist e) {
-			// TODO Auto-generated catch block
-			// if the program goes to this point fail  
+		} catch (EventFinished e) {
 			fail();
-			} catch (EventFinished e) {
-				// if the program goes to this point fail
-			    fail();
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				  //Remove the created objects in the database (cascade removing)   
-		          boolean b=testBL.removeEvent(ev);
-		           System.out.println("Finally "+b);          
-		        }
-		   }
+		}
+		
+	}
 }
