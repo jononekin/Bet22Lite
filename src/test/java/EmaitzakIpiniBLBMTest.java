003b7aafbdcfd56e83c0565a938426da167b8341
Agustin <76200509+AgusZ02@users.noreply.github.com>
2023-10-15 20:06:11 +0200

    Create EmaitzakIpiniBLBMTest.java

---
+++ src/test/java/EmaitzakIpiniBLBMTest.java
@@ -0,0 +1,75 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
+
+import org.junit.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Null;
+
+import businessLogic.BLFacade;
+import businessLogic.BLFacadeImplementation;
+import dataAccess.DataAccess;
+import domain.Event;
+import domain.Quote;
+import exceptions.EventNotFinished;
+import org.mockito.ArgumentCaptor;
+public class EmaitzakIpiniBLBMTest {
+    
+   
+    DataAccess daMock = Mockito.mock(DataAccess.class);
+    BLFacade sut = new BLFacadeImplementation();
+    
+
+    @Test
+    public void test1(){
+        try {
+            
+            Mockito.doThrow(new IllegalArgumentException()).when(daMock).EmaitzakIpini(null);
+            sut.EmaitzakIpini(null);
+            fail("No debería de ejecutar con parámetro null");
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void test2(){
+        Quote q = new Quote(2.0, "cuotaNoBD");
+        try {
+                Mockito.doThrow(new NullPointerException()).when(daMock).EmaitzakIpini(q);
+                ArgumentCaptor<Quote> argument = ArgumentCaptor.forClass(Quote.class);
+                assertThrows(NullPointerException.class,
+                ()->{
+                    sut.EmaitzakIpini(q);
+                    Mockito.verify(daMock, Mockito.times(1)).EmaitzakIpini(argument.capture());
+                    assertEquals(argument.getValue().getForecast(), q.getForecast());
+                    
+                });
+            } catch (EventNotFinished e) {
+                assertTrue(true);
+            }
+    }
+    @Test
+    public void test3(){
+        Quote q = new Quote(2.0, "cuotaEnBDFechaFutura");
+        DataAccess spy = spy(new DataAccess());
+        try {
+                Mockito.doThrow(new EventNotFinished()).when(daMock).EmaitzakIpini(q);
+                ArgumentCaptor<Quote> argument = ArgumentCaptor.forClass(Quote.class);
+                assertThrows(EventNotFinished.class,
+                ()->{
+                    sut.EmaitzakIpini(q);
+                    Mockito.verify(daMock, Mockito.times(1)).EmaitzakIpini(argument.capture());
+                    assertEquals(argument.getValue().getForecast(), q.getForecast());
+                    
+                });
+            } catch (EventNotFinished e) {
+                assertTrue(true);
+            }
+    }
+
+}
